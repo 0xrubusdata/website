58d5da6f63da4f7d1ad5b80542d33c3ab09b504c
0xrubusdata <0xrubusdatas@gmail.com>
2025-01-22 17:40:59 +0100

    Fixed: navbar components

--- components/organisms/MobileNavigationBar/MobileNavigationBar.tsx
+++ components/organisms/MobileNavigationBar/MobileNavigationBar.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styles from '../../../styles/MobileNavigationBar.module.css';
-import Links from '../../atoms/Links';
 import Button from '@/components/atoms/Button/Button';
+import { Link } from 'react-router-dom';
 import { LinkItem } from '@/components/types/atoms/LinkItem';
 
 interface MobileNavigationBarProps {
@@ -13,13 +13,21 @@ const MobileNavigationBar: React.FC<MobileNavigationBarProps> = ({ links, initia
   const [navbarExpanded, setNavbarExpanded] = useState(initialExpanded);
 
   const handleNavbarToggle = () => {
-    setNavbarExpanded(!navbarExpanded);
+    setNavbarExpanded((prevState) => !prevState);
   };
 
   return (
     <nav className={styles.mobileNavigationBar}>
       <Button label={navbarExpanded ? 'Hide Menu' : 'Show Menu'} onClick={handleNavbarToggle} />
-      {navbarExpanded && <Links links={links} />}
+      {navbarExpanded && (
+        <ul className={styles.linksList}>
+          {links.map((link, index) => (
+            <li key={index}>
+              <Link to={link.href}>{link.label}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </nav>
   );
 };
