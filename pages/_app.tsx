96b1b6ea94fd3ea28adbcdd2b6181d7a92a7ff48
0xrubusdata <0xrubusdatas@gmail.com>
2025-01-22 05:51:02 +0100

    Added: typewriter div for updates

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     return (
         <>
             <Head>
-                <title>Armind OS</title>
+                <title>ArmindOS - AI Agents Framework</title>
                 <link rel="icon" href="/icons8-favicon-16.ico" />
                 <meta name="description" content="Framework for agents AI development." />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,12 @@ const Home: FC = () => {
     return (
         <div className={styles.container}>
             <header className={styles.header}>
-                <Image src="/logo.png" alt="Logo Armind OS" width={100} height={100} />
-                <h1>Welcome to Armind OS Framework</h1>
+                <Image src="/logo.png" alt="Logo ArmindOS" width={100} height={100} />
+                <h1>Welcome to the ArmindOS Framework</h1>
             </header>
             <main className={styles.main}>
                 <p>
-                This framework allows you to build agent AI applications with ease and efficiency.
+                    This framework allows you to build agent AI applications with ease and efficiency.
                 </p>
                 <div className={styles.links}>
                     <a href="https://github.com/0xrubusdata/armind_os" target="_blank" rel="noopener noreferrer">
@@ -21,6 +21,11 @@ const Home: FC = () => {
                     <a href="https://x.com/Data0x88850" target="_blank" rel="noopener noreferrer">
                         X (Twitter)
                     </a>
+                </div>
+                <div className={styles.typewriter}>
+                    <p><em>01/22/2025</em></p>
+                    <p>** This website will be built by the AI agent 0xRubusData using the ArmindOS framework.</p>
+                    <p>** See you soon on this POC!</p>
                 </div>
             </main>
             <footer className={styles.footer}>
